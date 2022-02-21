5cf532d164165d508752f61be2a74dfbe0c65934
Zeb Mehring <zeb.mehring+github@gmail.com>
2022-02-21 14:50:55 -0500

    Add simple implementation for encoder block module
    Done according to the QANet paper. This implementation does not support multi-headed self-attention -- this will need to be added in a future change.

--- model/encoder_block.py
+++ model/encoder_block.py
@@ -1,9 +1,12 @@
 import torch
 
+from position_encoder import PositionEncoder
 
-class PositionEncoder(torch.nn.Module):
-    def __init__(self):
-        super(PositionEncoder, self).__init__()
+from torch.nn import Conv2d
+from torch.nn import ModuleList
+from torch.nn.functional import layer_norm
+from torch.nn.functional import relu
+from torch.nn.functional import softmax
 
 
 class EncoderBlock(torch.nn.Module):
@@ -21,5 +24,53 @@ class EncoderBlock(torch.nn.Module):
     See https://arxiv.org/pdf/1804.09541.pdf for more details.
     """
 
-    def __init__(self):
+    def __init__(self, hidden_size=128, num_convs=4, num_attn_heads=8):
+        """Constructs an encoder block module.
+
+        Args:
+          hidden_size [int]: the size of the feature representations used by the model;
+            also the output size
+          num_convs [int]: the number of convolutions to perform
+          num_attn_heads [int]: the number of heads to use in the self-attention layer
+        """
         super(EncoderBlock, self).__init__()
+
+        self.hidden_size = hidden_size
+
+        self.position_encoder = PositionEncoder()
+
+        self.convs = ModuleList([Conv2d(in_channels=hidden_size, out_channels=hidden_size, kernel_size=7)
+                                 for _ in range(num_convs)])
+
+        # TODO: Support multi-headed attention
+        self.Q = torch.nn.Linear(
+            in_features=hidden_size, out_features=hidden_size)
+        self.K = torch.nn.Linear(
+            in_features=hidden_size, out_features=hidden_size)
+        self.V = torch.nn.Linear(
+            in_features=hidden_size, out_features=hidden_size)
+
+        self.ff = torch.nn.Linear(
+            in_features=hidden_size, out_features=hidden_size)
+
+    def forward(self, x):
+        output = self.position_encoder(x)
+
+        for conv in self.convs:
+            residual = output
+            output = layer_norm(output, normalized_shape=self.hidden_size)
+            output = conv(output)
+            output += residual
+
+        residual = output
+        output = layer_norm(output, normalized_shape=self.hidden_size)
+        output = softmax(self.Q(output) @ self.K(output).T /
+                         torch.sqrt(self.hidden_size)) @ self.V(output)
+        output += residual
+
+        residual = output
+        output = layer_norm(output, normalized_shape=self.hidden_size)
+        output = relu(self.ff(output))
+        output += residual
+
+        return output
