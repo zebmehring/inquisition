2f9515d847da95ccb5340041c31f841d96fbe2c8
Andrew Gaut <agaut@rice11.stanford.edu>
2022-02-25 12:49:39 -0800

    fixed the mild changes to c2q attention

--- squad/qanet_layers.py
+++ squad/qanet_layers.py
@@ -71,15 +71,15 @@ class ContextQueryAttention(torch.nn.Module):
         batch_size, c_len, _ = c.size()
         q_len = q.size(1)
         s = self.get_similarity_matrix(c, q)  # (batch_size, c_len, q_len)
-        c_mask = c_mask.view(batch_size, c_len, 1)  # (batch_size, c_len, 1)
-        q_mask = q_mask.view(batch_size, 1, q_len)  # (batch_size, 1, q_len)
+        q_mask = q_mask.view(batch_size, q_len, 1)  # (batch_size, c_len, 1)
+        c_mask = c_mask.view(batch_size, 1, c_len)  # (batch_size, 1, q_len)
         s1 = masked_softmax(s, q_mask, dim=1)  # (batch_size, c_len, q_len)
         s2 = masked_softmax(s, c_mask, dim=2)  # (batch_size, c_len, q_len)
-
+        #pdb.set_trace()
         # (bs, c_len, q_len) x (bs, q_len, hid_size) => (bs, c_len, hid_size)
-        a = torch.bmm(s1, q)
+        a = torch.bmm(s1.transpose(1,2), q)
         # (bs, c_len, c_len) x (bs, c_len, hid_size) => (bs, c_len, hid_size)
-        b = torch.bmm(torch.bmm(s1, s2.transpose(1, 2)), c)
+        b = torch.bmm(torch.bmm(s1.transpose(1,2), s2), c)
 
         x = torch.cat([c, a, c * a, c * b], dim=2)  # (bs, c_len, 4 * hid_size)
 
