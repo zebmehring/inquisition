8dbcef6425bab36836497bfcc8950c7bb6f9ed14
Zeb Mehring <zeb.mehring+github@gmail.com>
2022-03-07 23:27:12 -0500

    Format qanet_layers.py (using autopep8)

--- squad/qanet_layers.py
+++ squad/qanet_layers.py
@@ -20,27 +20,32 @@ import pdb
 class StochasticDepth(torch.nn.Module):
     """ 
     """
+
     def __init__(self, l, L, p_L=0.9):
         super(StochasticDepth, self).__init__()
-   
-        self.p_l = 1 - (1 / L) * (1- p_L)
-        self.p_l_tensor = torch.tensor([self.p_l], dtype=torch.float32) 
+
+        self.p_l = 1 - (1 / L) * (1 - p_L)
+        self.p_l_tensor = torch.tensor([self.p_l], dtype=torch.float32)
 
     def forward(self, x):
-        # at training time, dropout the input 
+        # at training time, dropout the input
         if self.training:
-            keep = torch.bernoulli(self.p_l_tensor) 
-            return keep * x 
+            keep = torch.bernoulli(self.p_l_tensor)
+            return keep * x
         # at test time, we just multiply the output by p_keep
-        return self.p_l * x 
+        return self.p_l * x
+
 
 class DepthwiseSeparableConv1d(torch.nn.Module):
     """ Got help from https://www.paepper.com/blog/posts/depthwise-separable-convolutions-in-pytorch/. Still don't fully understands the 'groups' parameter """
+
     def __init__(self, in_channels, out_channels, kernel_size, padding):
         super(DepthwiseSeparableConv1d, self).__init__()
- 
-        self.depthwise_conv = nn.Conv1d(in_channels=in_channels, out_channels=in_channels, kernel_size=kernel_size, padding = padding, groups=in_channels)
-        self.pointwise_conv = nn.Conv1d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, padding=0)
+
+        self.depthwise_conv = nn.Conv1d(in_channels=in_channels, out_channels=in_channels,
+                                        kernel_size=kernel_size, padding=padding, groups=in_channels)
+        self.pointwise_conv = nn.Conv1d(
+            in_channels=in_channels, out_channels=out_channels, kernel_size=1, padding=0)
 
     def forward(self, x):
         return self.pointwise_conv(self.depthwise_conv(x))
@@ -70,8 +75,10 @@ class OutputLayer(torch.nn.Module):
         :param mask
         '''
         # both of the logits have shape (batch_size, seq_len, 1)
-        logits_start = self.start_span_linear(torch.cat((model_encoder_outputs[0], model_encoder_outputs[1]), dim=-1))
-        logits_end = self.start_span_linear(torch.cat((model_encoder_outputs[0], model_encoder_outputs[2]), dim=-1))
+        logits_start = self.start_span_linear(
+            torch.cat((model_encoder_outputs[0], model_encoder_outputs[1]), dim=-1))
+        logits_end = self.start_span_linear(
+            torch.cat((model_encoder_outputs[0], model_encoder_outputs[2]), dim=-1))
 
         # After squeezing, both logits have shape (batch_size, seq_len)
         log_p1 = masked_softmax(logits_start.squeeze(), mask, log_softmax=True)
@@ -80,11 +87,11 @@ class OutputLayer(torch.nn.Module):
         return log_p1, log_p2
 
 
-
 class SelfAttention(torch.nn.Module):
     """
     SelfAttention as described in Attention is All You Need.
     """
+
     def __init__(self, hidden_size, num_attn_heads):
         """
 
@@ -98,11 +105,15 @@ class SelfAttention(torch.nn.Module):
         # MAYBE PROJECT BEFORE INPUTTING TO MHA!
 
         # TEMPORARY!!! FOR TESTING
-        self.att = nn.MultiheadAttention(hidden_size, num_attn_heads, bias=False)
-        
-        self.w_q = nn.Linear(in_features=hidden_size, out_features=hidden_size, bias=False)
-        self.w_k = nn.Linear(in_features=hidden_size, out_features=hidden_size, bias=False)
-        self.w_v = nn.Linear(in_features=hidden_size, out_features=hidden_size, bias=False)
+        self.att = nn.MultiheadAttention(
+            hidden_size, num_attn_heads, bias=False)
+
+        self.w_q = nn.Linear(in_features=hidden_size,
+                             out_features=hidden_size, bias=False)
+        self.w_k = nn.Linear(in_features=hidden_size,
+                             out_features=hidden_size, bias=False)
+        self.w_v = nn.Linear(in_features=hidden_size,
+                             out_features=hidden_size, bias=False)
 
         """
         self.Qs = ModuleList([torch.nn.Linear(
@@ -124,7 +135,7 @@ class SelfAttention(torch.nn.Module):
         """
 
         :param x: has shape (batch_size, seq_len, hidden_size)
-	:param mask: tensor with shape (batch_size, seq_len) 
+        :param mask: tensor with shape (batch_size, seq_len) 
         :return:
         """
 
@@ -149,7 +160,8 @@ class SelfAttention(torch.nn.Module):
         """
         x = torch.transpose(x, 0, 1)
         #temp_x = temp_x.repeat(1,1, self.num_attn_heads)
-        x = self.att(self.w_q(x), self.w_k(x), self.w_v(x), key_padding_mask = ~mask)[0]
+        x = self.att(self.w_q(x), self.w_k(
+            x), self.w_v(x), key_padding_mask=~mask)[0]
         return torch.transpose(x, 0, 1)
 
 
@@ -179,58 +191,62 @@ class EncoderBlock(torch.nn.Module):
         """
         super(EncoderBlock, self).__init__()
 
-        self.num_convs = num_convs 
+        self.num_convs = num_convs
         self.drop_prob = drop_prob
         self.position_encoder = PositionEncoder(hidden_size, device, drop_prob)
-        
+
         self.hidden_size = hidden_size
 
         # Note: we set padding=3 below to maintain the dimensionality of the input
-	# We get this using the equation for L_out given in conv1d documentation
-	# othewise, each conv operation would reduce dimensionality of the input, which is probably not desirable
-	# since we would no longer have one vector per index in the sequence.
-        self.convs = ModuleList([DepthwiseSeparableConv1d(in_channels=hidden_size, out_channels=hidden_size, kernel_size=kernel_size, padding=(kernel_size - 1) // 2) for _ in range(num_convs)])
-        
-        self.layer_norms = ModuleList([nn.LayerNorm(normalized_shape=hidden_size) for _ in range(num_convs+2)])
+        # We get this using the equation for L_out given in conv1d documentation
+        # othewise, each conv operation would reduce dimensionality of the input, which is probably not desirable
+        # since we would no longer have one vector per index in the sequence.
+        self.convs = ModuleList([DepthwiseSeparableConv1d(in_channels=hidden_size, out_channels=hidden_size,
+                                kernel_size=kernel_size, padding=(kernel_size - 1) // 2) for _ in range(num_convs)])
+
+        self.layer_norms = ModuleList(
+            [nn.LayerNorm(normalized_shape=hidden_size) for _ in range(num_convs+2)])
 
         self.att = SelfAttention(hidden_size, num_attn_heads)
 
-
         # TA said to use kenrel_size = 1 and padding = 0 for these
-        self.ff = nn.Conv1d(in_channels = hidden_size, out_channels=hidden_size, kernel_size=1, padding=0) 
-        self.ff2 = nn.Conv1d(in_channels = hidden_size, out_channels=hidden_size, kernel_size=1, padding=0) 
+        self.ff = nn.Conv1d(in_channels=hidden_size,
+                            out_channels=hidden_size, kernel_size=1, padding=0)
+        self.ff2 = nn.Conv1d(in_channels=hidden_size,
+                             out_channels=hidden_size, kernel_size=1, padding=0)
 
     def forward(self, x, x_mask):
         """
         :param x: tensor with shape (batch_size, seq_len, hidden_size)
         :param x_mask: tensor with shape (batch_size, seq_len)  for which x_mask[i][j] = False if jth character of ith sequence is masked and True otherwise. We'll use this to zero out and get negative infinity where necessary.
         """
-        
-        output = self.position_encoder(x) # (batch_size, seq_len, hidden_size)
+
+        output = self.position_encoder(x)  # (batch_size, seq_len, hidden_size)
 
         for i, conv in enumerate(self.convs):
-            residual = output 
+            residual = output
             output = self.layer_norms[i](output)
             output = F.dropout(output, self.drop_prob, self.training)
-            output = conv(output.transpose(-1,-2)).transpose(-1,-2)# by transposing it, we get (batch_size, hidden_size, seq_len). Looking at the conv1d docs, this makes our in_channels equal to hidden_size as desired.
+            # by transposing it, we get (batch_size, hidden_size, seq_len). Looking at the conv1d docs, this makes our in_channels equal to hidden_size as desired.
+            output = conv(output.transpose(-1, -2)).transpose(-1, -2)
             output = F.relu(output)
             output = output + residual
 
-        residual = output 
-        output = self.layer_norms[self.num_convs](output) # (batch_size, seq_len, hidden_size)
+        residual = output
+        output = self.layer_norms[self.num_convs](
+            output)  # (batch_size, seq_len, hidden_size)
         output = F.dropout(output, self.drop_prob, self.training)
         output = self.att(output, x_mask)
         output = output + residual
 
-        residual = output 
+        residual = output
         output = self.layer_norms[self.num_convs+1](output)
         output = F.dropout(output, self.drop_prob, self.training)
-        output = relu(self.ff(output.transpose(-1,-2)).transpose(-1,-2))
-        output = self.ff2(output.transpose(-1,-2)).transpose(-1,-2)
+        output = relu(self.ff(output.transpose(-1, -2)).transpose(-1, -2))
+        output = self.ff2(output.transpose(-1, -2)).transpose(-1, -2)
         output = output + residual
 
         return output
-
 
 
 class PositionEncoder(torch.nn.Module):
@@ -251,14 +267,15 @@ class PositionEncoder(torch.nn.Module):
 
         self.drop_prob = drop_prob
 
-
-        freq_indices = torch.arange(hidden_size//2)#.repeat_interleave(2)
+        freq_indices = torch.arange(hidden_size//2)  # .repeat_interleave(2)
         frequencies = torch.pow(10000, (2*freq_indices)/hidden_size)
 
         positions = torch.arange(max_seq_len).reshape(-1, 1)
-        positions = positions.repeat(1, hidden_size // 2) # shape (max_seq_len, hidden-size // 2). These are the t values in p_t
+        # shape (max_seq_len, hidden-size // 2). These are the t values in p_t
+        positions = positions.repeat(1, hidden_size // 2)
 
-        self.position_encodings = torch.zeros((max_seq_len, hidden_size)).to(device)
+        self.position_encodings = torch.zeros(
+            (max_seq_len, hidden_size)).to(device)
 
         self.position_encodings[:, 0::2] = torch.sin(positions / frequencies)
         self.position_encodings[:, 1::2] = torch.cos(positions / frequencies)
@@ -272,8 +289,7 @@ class PositionEncoder(torch.nn.Module):
         # note that we only get the first seq_len position encodings (since max_seq_len
         # may be greater than seq_len)
         output = self.position_encodings[:x.shape[1]] + x
-        return F.dropout(output, self.drop_prob, self.training) 
-
+        return F.dropout(output, self.drop_prob, self.training)
 
 
 class Embedding(nn.Module):
@@ -287,14 +303,18 @@ class Embedding(nn.Module):
         hidden_size (int): Size of hidden activations.
         drop_prob (float): Probability of zero-ing out activations
     """
+
     def __init__(self, word_vectors, character_vectors, hidden_size, drop_prob):
         super(Embedding, self).__init__()
         self.drop_prob = drop_prob
-        self.char_embed = nn.Embedding.from_pretrained(character_vectors, freeze=False)
+        self.char_embed = nn.Embedding.from_pretrained(
+            character_vectors, freeze=False)
         self.word_embed = nn.Embedding.from_pretrained(word_vectors)
-        #self.conv1d = nn.Conv1d(in_channels=character_vectors.shape[-1], out_channels=hidden_size // 2, kernel_size=3, padding=1) # not sure on kernel size
-        self.conv = nn.Conv2d(in_channels=character_vectors.shape[-1], out_channels = hidden_size, kernel_size=(1,5))
-        self.conv_projection = nn.Conv1d(in_channels=word_vectors.shape[-1] + hidden_size, out_channels=hidden_size, kernel_size=1, padding=0, bias=False)
+        # self.conv1d = nn.Conv1d(in_channels=character_vectors.shape[-1], out_channels=hidden_size // 2, kernel_size=3, padding=1) # not sure on kernel size
+        self.conv = nn.Conv2d(
+            in_channels=character_vectors.shape[-1], out_channels=hidden_size, kernel_size=(1, 5))
+        self.conv_projection = nn.Conv1d(
+            in_channels=word_vectors.shape[-1] + hidden_size, out_channels=hidden_size, kernel_size=1, padding=0, bias=False)
         self.hwy = HighwayEncoder(2, hidden_size)
 
         # self.test = PositionEncoder(hidden_size)
@@ -311,23 +331,33 @@ class Embedding(nn.Module):
         :return:
         """
         word_idxs, char_idxs = x
-        word_emb = self.word_embed(word_idxs)   # (batch_size, seq_len, word_embed_size)
-        char_emb = self.char_embed(char_idxs) # (batch_size, seq_len, word_len, char_embed_size)
+        # (batch_size, seq_len, word_embed_size)
+        word_emb = self.word_embed(word_idxs)
+        # (batch_size, seq_len, word_len, char_embed_size)
+        char_emb = self.char_embed(char_idxs)
 
-        char_emb = char_emb.permute(0, 3, 1, 2) # shape (batch_size, char_embed_size, seq_len, word_len)
-        char_emb = self.conv(char_emb) # shape (batch_size, char_embed_size, seq_len, word_len)
-        char_emb = F.relu(char_emb) # shape (batch_size, hidden_size, seq_len, word_len) 
-        char_emb = torch.max(char_emb, dim=-1)[0] # (batch_size, hidden_size, seq_len)
-        char_emb = char_emb.permute(0,2,1) # shape (batch_size, seq_len, hidden_size)
+        # shape (batch_size, char_embed_size, seq_len, word_len)
+        char_emb = char_emb.permute(0, 3, 1, 2)
+        # shape (batch_size, char_embed_size, seq_len, word_len)
+        char_emb = self.conv(char_emb)
+        # shape (batch_size, hidden_size, seq_len, word_len)
+        char_emb = F.relu(char_emb)
+        # (batch_size, hidden_size, seq_len)
+        char_emb = torch.max(char_emb, dim=-1)[0]
+        # shape (batch_size, seq_len, hidden_size)
+        char_emb = char_emb.permute(0, 2, 1)
         char_emb = F.dropout(char_emb, self.drop_prob // 2, self.training)
 
-        word_emb = F.dropout(word_emb, self.drop_prob, self.training) # shape (batch_size, seq_len, word_embed_size)
+        # shape (batch_size, seq_len, word_embed_size)
+        word_emb = F.dropout(word_emb, self.drop_prob, self.training)
 
-        emb = torch.cat((word_emb, char_emb), dim=2) # (batch_size, seq_len, word_emb_size hidden_size)
-        emb = self.conv_projection(emb.transpose(-1,-2)).transpose(-1,-2) # (batch_size, seq_len, hidden_size)
+        # (batch_size, seq_len, word_emb_size hidden_size)
+        emb = torch.cat((word_emb, char_emb), dim=2)
+        # (batch_size, seq_len, hidden_size)
+        emb = self.conv_projection(emb.transpose(-1, -2)).transpose(-1, -2)
 
         emb = self.hwy(emb)   # (batch_size, seq_len, hidden_size)
-  
+
         # emb = self.test(emb)
 
         # Str8AStudent
