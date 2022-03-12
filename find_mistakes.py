32b79555066dec15f5210d09e64a74b5027363e2
Zeb Mehring <zeb.mehring+github@gmail.com>
2022-03-12 17:23:51 -0500

    Add small script for analyzing model mistakes on dev set

---
+++ find_mistakes.py
@@ -0,0 +1,48 @@
+from csv import reader
+from ujson import load
+
+with open('./squad/data/dev_eval.json') as f:
+    gold_dict = load(f)
+
+goldens = {}
+for d in gold_dict.values():
+    goldens[d['uuid']] = set(d['answers'])
+
+with open('./squad/data/dev-submission.csv') as f:
+    predictions = {}
+    for row in f:
+        uuid, answer = row.split(',', maxsplit=1)
+        try:
+            int(uuid, base=16)
+        except:
+            continue
+        answer = answer.strip()
+        predictions[uuid] = answer
+
+total_incorrect = 0
+empty_golden_nonempty_prediction = 0
+empty_prediction_nonempty_golden = 0
+bad_guess = 0
+subspans = 0
+for uuid, prediction in predictions.items():
+    if not prediction and not goldens[uuid]:
+        continue
+    total_incorrect += 1
+    if not prediction or not goldens[uuid]:
+        empty_golden_nonempty_prediction += 1 if prediction and not goldens[uuid] else 0
+        empty_prediction_nonempty_golden += 1 if goldens[uuid] and not prediction else 0
+        continue
+    if prediction not in goldens[uuid]:
+        bad_guess += 1
+        if any(prediction in golden or golden in prediction for golden in goldens[uuid]):
+            subspans += 1
+        else:
+            print(f'incorrect prediction \'{prediction}\', '
+                  f'expected one of {goldens[uuid]}')
+
+print('')
+print(f'total incorrect: {total_incorrect}')
+print(f'empty golden nonempty prediction: {empty_golden_nonempty_prediction}')
+print(f'empty prediction nonempty golden: {empty_prediction_nonempty_golden}')
+print(f'bad guesses: {bad_guess}')
+print(f'subspans: {subspans}')
